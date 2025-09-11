74de3bdd8564a8465a9947c1ffe2d5be20c4e7d9
Jeann Lucas <jeann.lucas@outlook.com>
2025-09-11 12:11:52 -0300

    removendo as descriçoes da area de atuacao

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -11,48 +11,48 @@ const projectData = [
     id: 1,
     image: card1,
     title: "Desvio de septo e sinusite",
-    description:
-      "O desvio de septo ocorre quando a parede que separa as narinas está torta ou desalinhada, dificultando a passagem de ar. Já a sinusite é uma inflamação dos seios da face, geralmente causada por infecções ou alergias. O desvio de septo pode aumentar o risco de sinusite por dificultar a drenagem adequada do muco.",
+    // description:
+    //   "O desvio de septo ocorre quando a parede que separa as narinas está torta ou desalinhada, dificultando a passagem de ar. Já a sinusite é uma inflamação dos seios da face, geralmente causada por infecções ou alergias. O desvio de septo pode aumentar o risco de sinusite por dificultar a drenagem adequada do muco.",
     link: "https://wa.me/5544999730804?text=Ola, gostaria de agendar uma consulta com o Dr. Jonas Tamanini, por favor.",
   },
   {
     id: 2,
     image: card2,
     title: "Roncos noturnos e apneia do sono",
-    description:
-      "Os roncos noturnos acontecem devido à vibração dos tecidos da garganta durante a respiração, muitas vezes causados por obstruções nas vias aéreas. Já a apneia do sono é uma condição mais séria, em que a respiração para ou fica muito superficial por alguns segundos durante o sono, afetando a qualidade do descanso e a saúde geral.",
+    // description:
+    //   "Os roncos noturnos acontecem devido à vibração dos tecidos da garganta durante a respiração, muitas vezes causados por obstruções nas vias aéreas. Já a apneia do sono é uma condição mais séria, em que a respiração para ou fica muito superficial por alguns segundos durante o sono, afetando a qualidade do descanso e a saúde geral.",
     link: "https://wa.me/5544999730804?text=Ola, gostaria de agendar uma consulta com o Dr. Jonas Tamanini, por favor.",
   },
   {
     id: 3,
     image: card3,
     title: "Amigdalites e cirurgia de amígdalas",
-    description:
-      "A amigdalite é uma inflamação das amígdalas, geralmente causada por vírus ou bactérias, que provoca dor de garganta, febre e dificuldade para engolir. Quando as infecções são frequentes ou causam complicações, pode ser indicada a cirurgia de remoção das amígdalas, chamada amigdalectomia, para melhorar a qualidade de vida e prevenir novos episódios.",
+    // description:
+    //   "A amigdalite é uma inflamação das amígdalas, geralmente causada por vírus ou bactérias, que provoca dor de garganta, febre e dificuldade para engolir. Quando as infecções são frequentes ou causam complicações, pode ser indicada a cirurgia de remoção das amígdalas, chamada amigdalectomia, para melhorar a qualidade de vida e prevenir novos episódios.",
     link: "https://wa.me/5544999730804?text=Ola, gostaria de agendar uma consulta com o Dr. Jonas Tamanini, por favor.",
   },
   {
     id: 4,
     image: card4,
     title: "Cirurgias de perfuração de tímpano",
-    description:
-      "A perfuração do tímpano pode causar dor, perda auditiva e infecções frequentes. Em alguns casos, é necessário realizar uma cirurgia chamada timpanoplastia para fechar o orifício e restaurar a audição.",
+    // description:
+    //   "A perfuração do tímpano pode causar dor, perda auditiva e infecções frequentes. Em alguns casos, é necessário realizar uma cirurgia chamada timpanoplastia para fechar o orifício e restaurar a audição.",
     link: "https://wa.me/5544999730804?text=Ola, gostaria de agendar uma consulta com o Dr. Jonas Tamanini, por favor.",
   },
   {
     id: 5,
     image: card5,
     title: "Avaliação de perda auditiva e zumbido",
-    description:
-      "A perda auditiva e o zumbido podem afetar a comunicação e a qualidade de vida. A avaliação especializada identifica a causa e orienta tratamentos para melhorar a audição e reduzir os sintomas.",
+    // description:
+    //   "A perda auditiva e o zumbido podem afetar a comunicação e a qualidade de vida. A avaliação especializada identifica a causa e orienta tratamentos para melhorar a audição e reduzir os sintomas.",
     link: "https://wa.me/5544999730804?text=Ola, gostaria de agendar uma consulta com o Dr. Jonas Tamanini, por favor.",
   },
   {
     id: 6,
     image: card6,
-    title: "Tratamendo de otites",
-    description:
-      "As otites são infecções ou inflamações do ouvido que causam dor e desconforto. O tratamento adequado, que pode incluir medicamentos ou cuidados locais, ajuda a aliviar os sintomas e prevenir complicações.",
+    title: "Tratamento de otites",
+    // description:
+    //   "As otites são infecções ou inflamações do ouvido que causam dor e desconforto. O tratamento adequado, que pode incluir medicamentos ou cuidados locais, ajuda a aliviar os sintomas e prevenir complicações.",
     link: "https://wa.me/5544999730804?text=Ola, gostaria de agendar uma consulta com o Dr. Jonas Tamanini, por favor.",
   },
 ];
